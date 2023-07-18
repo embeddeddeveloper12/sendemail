ce1bdcdacd336e0c9f93a93cd2d16e3c13881108
Amarnath Reddy <73329067+embeddeddeveloper12@users.noreply.github.com>
2023-07-18 09:40:33 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+$to = "amarnath13041304@gmail.com";
+$subject = "Hello Amar";
+$txt = "Testing Email Sending";
+$header = " From: akashreddy8776@gmail.com";
+mail($to,$subject,$txt,$header);
+?>
